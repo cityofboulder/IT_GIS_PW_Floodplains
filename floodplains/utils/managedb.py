e05c86ca19fb5054db95f7f08d1a7c2b97c86346
Jesse Nestler <jesse.nestler@colorado.edu>
2020-07-10 11:03:22 -0600

    Add logic to create_version function

--- floodplains/utils/managedb.py
+++ floodplains/utils/managedb.py
@@ -1,4 +1,5 @@
 import cryptography
+import arcpy
 
 
 def decrypt(key: str, token: str):
@@ -23,8 +24,30 @@ def decrypt(key: str, token: str):
     return decrypted.decode("utf-8")
 
 
-def create_version():
-    pass
+def create_version(version_kwargs: dict):
+    """Creates a version using the dict variables defined in the project
+    config.
+
+    The tool uses status codes provided by ESRI to keep trying a version
+    creation until the tool succeeds (status code = 4). Status codes can
+    be found here:
+
+    https://pro.arcgis.com/en/pro-app/arcpy/classes/result.htm
+
+    Parameters
+    ----------
+    version_kwargs : dict
+        Parameters required for the arcpy.CreateVersion_management
+        function
+    """
+    # TODO: Add logging
+    status = 0
+    while status != 4:
+        result = arcpy.CreateVersion_management(**version_kwargs)
+        status = result.status
+        if status != 4:
+            # TODO: Add logging
+            pass
 
 
 def create_versioned_connection():
