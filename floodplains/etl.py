dd10b8de96680a4ec8fcdcc8da85362da95eb753
Jesse Nestler <jesse.nestler@colorado.edu>
2020-07-13 13:01:02 -0600

    Add outline of script process

--- floodplains/etl.py
+++ floodplains/etl.py
@@ -28,3 +28,27 @@ def main():
     city_union = arcgis.geometry.union(
         spatial_ref=sr, geometries=city_geoms, gis=anon_gis)
     geom_filter = arcgis.geometry.filters.intersects(city_union, sr=sr)
+
+    # EXTRACT
+    # Step 3a: Extract LOMRs based on the desired criteria
+    # Step 3b: Find the most recent update to our internal floodplains through
+    # a connection to sde (using ADOPTDATE)
+    # Step 3c: Identify LOMRs that are more recent than the most recent
+    # floodplain update, and remove potential duplicates
+    # Step 4: If no new lomrs exist, send an email to the steward
+    # otherwise, extract new sfha delineations using the lomr boundaries
+
+    # TRANSFORM
+    # Step 5: Create a new versioned connection for city floodplains
+    # Step 6a: Transform sfha delineations natively (dicts or pandas)
+    # Step 6b: Dissolve new delins based on COB standards
+
+    # LOAD
+    # Step 7a: Make edits to the version
+    # Step 7b: Cut existing floodplains with LOMR boundaries
+    # Step 7c: Make existing floodplains inside LOMR bounds "Inactive"
+    # Step 7d: Add the transformed sfhas into the version
+
+    # NOTIFY
+    # Step 8a: Notify steward of new version edits
+    # Step 8b: Notify SMEs that edits are pending and new LOMRs are available
