ccabd1e81ea98b01e62f7744c7e61f0ab4d03f01
Jesse Nestler <jesse.nestler@colorado.edu>
2020-07-13 12:05:22 -0600

    Add feature service urls

--- floodplains/etl.py
+++ floodplains/etl.py
@@ -1,8 +1,20 @@
 import floodplains.config as config
 
+import arcgis
+
+
 # Initiate a logger for etl
 log = config.logging.getLogger(__name__)
 
 
 def main():
-    pass
+    # Step 1: Identify relevant feature services
+    urls = {"city": ("https://maps.bouldercolorado.gov/arcgis/rest/services/"
+                     "plan/CityLimits/MapServer/0"),
+            "nfhl": ("https://hazards.fema.gov/gis/nfhl/rest/services/"
+                     "public/NFHL/MapServer")}
+
+    city = arcgis.features.FeatureLayer(urls["city"])
+    nfhl = arcgis.features.FeatureLayerCollection(urls["nfhl"])
+    lomr = nfhl.layers[1]
+    sfha = nfhl.layers[27]
