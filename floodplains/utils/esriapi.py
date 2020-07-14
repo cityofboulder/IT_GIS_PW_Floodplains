7c044035c52decca3e8207e7b9a8aa1d97578036
Jesse Nestler <jesse.nestler@colorado.edu>
2020-07-13 18:53:42 -0600

    Add extract_sfha function

--- floodplains/utils/esriapi.py
+++ floodplains/utils/esriapi.py
@@ -69,6 +69,7 @@ def query_lomr(in_layer: arcgis.features.layer.FeatureLayer,
     arcgis.features.layer.FeatureLayer
         An ESRI FeatureLayer
     """
+    # Query the feature service
     lomrs = in_layer.query(where=clause,
                            geometry_filter=g_filter,
                            out_sr=sr)
@@ -80,3 +81,73 @@ def query_lomr(in_layer: arcgis.features.layer.FeatureLayer,
     lomrs = arcgis.features.FeatureSet.from_dataframe(temp)
 
     return lomrs
+
+
+def extract_sfha(in_layer: arcgis.features.layer.FeatureLayer,
+                 boundaries: arcgis.features.layer.FeatureLayer,
+                 clause: str, out_fields: list, sr: int):
+    """Extracts all the SFHA floodplains that are within some boundaries
+    into a pandas dataframe.
+
+    This package either requires shapely or arcpy, depending on the OS.
+    The output pandas dataframe is spatially enabled, and contains no
+    duplicate geometries based on FEMA's ID scheme. Duplicattes can
+    occur periodically because LOMRs can overlap.
+
+    Parameters
+    ----------
+    in_layer : arcgis.features.layer.FeatureLayer
+        A feature layer derived from a feature or map service endpoint
+    boundaries : arcgis.features.layer.FeatureLayer
+        The boundaries in which to evaluate "insidedness"
+    clause : str
+        Conditions used to restrict output, written in SQL syntax (e.g.
+        "DFIRM_ID = '08013C'")
+    out_fields : list
+        The fields to output as columns in the spatial dataframe
+    sr : int
+        The output spatial reference
+
+    Returns
+    -------
+    pandas.DataFrame
+        With duplicate geometries removed
+    dict
+        A dictionary summarizing number of SFHAs in a given polygon
+        based on ID
+    """
+    # Query the feature service
+    all_sfha = in_layer.query(where=clause, out_fields=out_fields, out_sr=sr)
+
+    # Create an empty dataframe with the same schema as the output
+    all_sfha_sdf = all_sfha.sdf
+    subset = all_sfha_sdf.copy()
+    subset.drop(list(range(len(subset))), inplace=True)
+
+    # Loop through the LOMR boundaries and ID all SFHAs that are inside
+    catalog = {}
+    for lomr in boundaries.features:
+        case = lomr.attributes['CASE_NO']
+        g = arcgis.geometry.Geometry(lomr.geometry)
+
+        # buffer LOMR geom by one foot to avoid topological errors where polys
+        # share an edge
+        buf = g.buffer(1)
+        count = 0
+
+        for row in all_sfha.features:
+            area_id = row.attributes['FLD_AR_ID']
+            f = arcgis.geometry.Geometry(row.geometry)
+            if buf.contains(f):
+                subset = subset.append(
+                    all_sfha_sdf[all_sfha_sdf['FLD_AR_ID'] == area_id],
+                    ignore_index=True)
+                count += 1
+
+        # log counts by LOMR Case Number
+        catalog[case] = count
+
+    # drop any rows that represent duplicate flood areas
+    subset.drop_duplicates(subset=['FLD_AR_ID'], inplace=True)
+
+    return subset, catalog
