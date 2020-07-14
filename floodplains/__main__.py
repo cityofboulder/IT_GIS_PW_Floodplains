f5415eff9059c00abbf5c20d6d1777722e5eab48
Jesse Nestler <jesse.nestler@colorado.edu>
2020-07-14 12:50:38 -0600

    Alter logic in try block
    If nothing needed to be extracted from FEMA, the script sends an email
    from __main__ and the script finishes. Otherwise, the script moves on to
    the transform and load steps of the process.

--- floodplains/__main__.py
+++ floodplains/__main__.py
@@ -1,5 +1,6 @@
 import floodplains.etl as etl
 import floodplains.config as config
+import floodplains.utils.email as email
 from floodplains.utils.managedisk import list_files
 
 # Initiate a logger for __main__
@@ -7,8 +8,20 @@ log = config.logging.getLogger(__name__)
 
 if __name__ == "__main__":
     try:
-        etl.main()
+        log.info("Initiating extraction.")
+        output = etl.extract()
+        if output:
+            # log.info("Initiating transformation.")
+            # etl.transform(output)
+            # log.info("Initiating load.")
+            # etl.load()
+            pass
+        else:
+            log.info("Notifying steward that no changes were made in Boulder.")
+            body = email.email_body("No new LOMRs have been added in Boulder.")
+            email.send_email(sender=config.sender, password=config.password,
+                             recipients=config.steward, body=body)
     except Exception:
-        log.exception("Something prevented the script from running")
+        log.exception("Something prevented the script from running.")
     finally:
         list_files(['.sde'], delete=True)
