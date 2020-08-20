6555454b366dd5eb4ff7d4443324b9936ebb6510
= <nestlerj@bouldercolorado.gov>
2020-08-20 11:35:05 -0600

    Initialize various functions of the module

--- floodplains/utils/editdb.py
+++ floodplains/utils/editdb.py
@@ -1,3 +1,29 @@
 import floodplains.config as config
 
 log = config.logging.getLogger(__name__)
+
+
+def _cut_polys():
+    # Cuts the city flood polygons
+    pass
+
+
+def _inactivate_polys():
+    # Make all existing polys inside the LOMR boundaries 'Inactive'
+    pass
+
+
+def _add_new_polys():
+    # Add new transformed polygons from FEMA
+    pass
+
+
+def _dissolve_polys():
+    # Dissolve polygons inside the new LOMR by DRAINAGE, FEMAZONE, FLOODPLAIN,
+    # and LIFECYCLE
+    pass
+
+
+def perform_edits():
+    # Make all edits to the city floodplains
+    pass
