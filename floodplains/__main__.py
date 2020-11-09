bcd864b9fae39f98e8def6e34195d859814a3f3d
Jesse Nestler <jesse.nestler@colorado.edu>
2020-11-09 13:27:58 -0700

    Add URL tests

--- floodplains/__main__.py
+++ floodplains/__main__.py
@@ -1,3 +1,5 @@
+import requests
+
 import floodplains.config as config
 import floodplains.etl as etl
 import floodplains.utils.email as email
@@ -9,19 +11,31 @@ log = config.logging.getLogger(__name__)
 
 if __name__ == "__main__":
     try:
-        log.info("Removing old edit version.")
-        remove_version(config.edit_conn, config.version_name)
-        log.info("Initiating extraction.")
-        new_sfhas, new_lomrs = etl.extract()
-        if new_sfhas is not None:
-            log.info("Initiating transformation.")
-            transformed = etl.transform(new_sfhas, new_lomrs)
-            # log.info("Initiating load.")
-            # etl.load()
-            pass
+        log.info("Testing REST Endpoints.")
+        offline = list(filter(lambda u: requests.get(
+            u, {"f": "pjson"}).status_code != 200, config.urls))
+        if not offline:
+            log.info("Removing old edit version.")
+            remove_version(config.edit_conn, config.version_name)
+            log.info("Initiating extraction.")
+            new_sfhas, new_lomrs = etl.extract()
+            if new_sfhas is not None:
+                log.info("Initiating transformation.")
+                transformed = etl.transform(new_sfhas, new_lomrs)
+                # log.info("Initiating load.")
+                # etl.load()
+                pass
+            else:
+                log.info("No changes were made in Boulder.")
+                body = email.email_body("No changes were made in Boulder.")
+                email.send_email(sender=config.sender,
+                                 password=config.password,
+                                 recipients=config.steward, body=body)
         else:
-            log.info("Notifying steward that no changes were made in Boulder.")
-            body = email.email_body("No new LOMRs have been added in Boulder.")
+            log.error("Offline URLs: " + ", ".join(u for u in offline))
+            body = email.email_body("The following URLs are offline:<br><br>" +
+                                    ", ".join(u for u in offline) +
+                                    "<br><br>Try again later.")
             email.send_email(sender=config.sender, password=config.password,
                              recipients=config.steward, body=body)
     except Exception:
